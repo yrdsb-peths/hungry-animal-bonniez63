f5f1a16d13bc02c9d36dbe609b8bf49d9c62ffd5
bonniez63 <131053816+bonniez63@users.noreply.github.com>
2023-05-08 08:35:24 -0400

    Created a World and added Snake.

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The World our villain lives in.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Bonnie Zhuang
+ * @version May 2023
  */
 public class MyWorld extends World
 {
@@ -17,5 +17,8 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Snake snake = new Snake();
+        addObject(snake, 300, 200);
     }
 }
---
+++ Snake.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Snake, our villain.
+ * 
+ * @author Bonnie Zhuang
+ * @version May 2023
+ */
+public class Snake extends Actor
+{
+    /**
+     * Act - do whatever the Snake wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+    }
+}
