e4ef4af45b5987f97d66f58466768309e9751f63
bonniez63 <131053816+bonniez63@users.noreply.github.com>
2023-05-18 22:04:00 -0400

    Changed snake image, added animations and title screen.

--- Snake.java
+++ Snake.java
@@ -14,18 +14,65 @@ public class Snake extends Actor
      */
     
     GreenfootSound snakeSound = new GreenfootSound("snake-hissing-6092.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[7];
+    GreenfootImage[] idleLeft = new GreenfootImage[7]; 
+    
+    String facing = "left";
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    public Snake()
+    {
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/snake_idle/idle" + i + ".png");
+            idleLeft[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/snake_idle/idle" + i + ".png");
+            idleRight[i].mirrorHorizontally();
+            idleRight[i].scale(100, 100);
+        }
+        
+        setImage(idleLeft[0]);
+        animationTimer.mark();
+    }
+    
+    int imageIndex = 0;
+    public void animateSnake()
+    {
+        if(animationTimer.millisElapsed() < 2000)
+        {
+            animationTimer.mark();
+        }
+        if(facing.equals("left"))
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+        else
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+    }
+    
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("left"))
         {
             move(-3);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
             move(3);
+            facing = "right";
         }
         eat();
+        animateSnake();
     }
     
     public void eat()
---
+++ TitleScreen.java
@@ -0,0 +1,49 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Snake", 75);
+
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+
+        addObject(titleLabel, 300, 75);
+        prepare();
+    }
+
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Snake snake = new Snake();        
+        addObject(snake,300, 200);
+        Label label = new Label("Use << and >> to Move", 50);
+        addObject(label,300,300);
+        Label label2 = new Label("Click Space to Start", 60);
+        addObject(label2,300,350);
+    }
+}
